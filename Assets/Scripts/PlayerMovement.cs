42beb09f252bf3c1c0bb3a7eb5e8551a61a521d3
YH <873386843@qq.com>
2024-09-05 17:28:07 +0800

    03-007-done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControlls controls;
+    private CharacterController characterController;
+    private Animator anim;
+
+    [Header("Movement Info")]
+    [SerializeField] private float walkSpeed;
+    private float verticalVelocity;
+    private Vector3 movementDirection;
+
+    [Header("Aim Infor")]
+    [SerializeField] private Transform aim;
+    [SerializeField] private LayerMask aimLayerMask;
+    private Vector3 lookingDirection;
+
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
+
+    private void Awake()
+    {
+        controls = new PlayerControlls();
+
+        //这段代码的作用是，fire事件执行时，给定输入事件的上下文，函数为shoot
+        controls.Character.Fire.performed += context => Shoot();
+
+        //下面的代码作用就是movement执行和取消时，对事件的上下文赋值
+        //当按下wasd时，给与vector2的值，松开时，就是vector2.zero
+        controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
+        controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
+
+        controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
+        controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
+
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+        AimTowardsMouse();
+        AnimatorControllers();
+    }
+
+    private void AnimatorControllers()
+    {
+        //这里要使用向量的点积
+        float xVelocity = Vector3.Dot(movementDirection.normalized, transform.right);
+        float zVelocity = Vector3.Dot(movementDirection.normalized, transform.forward);
+
+        anim.SetFloat("xVelocity", xVelocity, 0.05f, Time.deltaTime);
+        anim.SetFloat("zVelocity", zVelocity, 0.05f, Time.deltaTime);
+    }
+
+    private void AimTowardsMouse()
+    {
+        //这个函数的作用，就是让玩家移至朝向鼠标的位置
+        //camera实际上有一个视野是一个四棱锥，称为视锥体
+        //在屏幕上点击的位置虽然是个二维坐标，实际上就是这个视椎体的底部上面的区域
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
+
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+        ApplyGravity();
+
+        //如果存在输入
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * walkSpeed);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (characterController.isGrounded == false)
+        {
+            verticalVelocity -= 9.81f * Time.deltaTime;
+            movementDirection.y = verticalVelocity;
+        }
+        else
+            verticalVelocity = -0.5f;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
