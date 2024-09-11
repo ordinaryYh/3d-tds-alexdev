d9b0579c20469c6a21ea3cbf75230339105e4c29
YH <873386843@qq.com>
2024-09-11 18:15:35 +0800

    06-009-done

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFX;
+
     private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFX(collision);
         Destroy(gameObject);
+    }
+
+    private void CreateImpactFX(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            //下面的代码是只在第一次接触的点，触发fx特效
+            ContactPoint contact = collision.contacts[0];
+            GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
+
+            Destroy(newImpactFX, 1);
+        }
     }
 }
