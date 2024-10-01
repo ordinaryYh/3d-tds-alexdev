06abfc5ccf58fb758525db5837c1ae4df815fa2b
ordinaryYh <873386843@qq.com>
2024-10-01 14:08:31 +0800

    15-003-done

--- Assets/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -4,24 +4,73 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+    [SerializeField] private Transform lastLevelPart;
     [SerializeField] private List<Transform> levelParts;
+    private List<Transform> currentLevelParts;
     [SerializeField] private SnapPoint nextSnapPoint;
 
+    [Space]
+    [SerializeField] private float generationCooldown;
+    private float cooldownTimer;
+    private bool generationOver;
+
+    private void Start()
+    {
+        currentLevelParts = new List<Transform>(levelParts);
+    }
+
+    private void Update()
+    {
+        if (generationOver == true)
+            return;
+
+        cooldownTimer -= Time.deltaTime;
+
+        if (cooldownTimer < 0)
+        {
+            //每次生成，列表都会减少一个，所有都生成完后，就生成finish的levelpart
+            //然后就结束生成
+            if (currentLevelParts.Count > 0)
+            {
+                cooldownTimer = generationCooldown;
+                GenerateNextLevelPart();
+            }
+            else if (generationOver == false)
+            {
+                FinishGeneration();
+            }
+        }
+    }
+
+    //生成最后一个levelpart，这样就停止生成关卡了
+    private void FinishGeneration()
+    {
+        generationOver = true;
+
+        Transform levelPart = Instantiate(lastLevelPart);
+        LevelPart levelPartScript = levelPart.GetComponent<LevelPart>();
+
+        levelPartScript.SnapAndAlignPartTo(nextSnapPoint);
+    }
 
     [ContextMenu("Create next level part")]
     private void GenerateNextLevelPart()
     {
         Transform newPart = Instantiate(ChooseRandomPart());
+        LevelPart levelPart = newPart.GetComponent<LevelPart>();
 
+        levelPart.SnapAndAlignPartTo(nextSnapPoint);
+
+        nextSnapPoint = levelPart.GetExitPoint();
     }
 
     private Transform ChooseRandomPart()
     {
-        int randomIndex = Random.Range(0, levelParts.Count);
+        int randomIndex = Random.Range(0, currentLevelParts.Count);
 
-        Transform choosenPart = levelParts[randomIndex];
+        Transform choosenPart = currentLevelParts[randomIndex];
 
-        levelParts.RemoveAt(randomIndex);
+        currentLevelParts.RemoveAt(randomIndex);
 
         return choosenPart;
     }
---
+++ Assets/Scripts/LevelGeneration/LevelPart.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPart : MonoBehaviour
+{
+    public void SnapAndAlignPartTo(SnapPoint _targetSnapPoint)
+    {
+        //相当是用另一个level的enter对准另一个level的exit
+        SnapPoint enterPoint = GetEnterPoint();
+
+        AlighTo(enterPoint, _targetSnapPoint);//首先要先进行旋转，然后才能连接
+        SnapTo(enterPoint, _targetSnapPoint);
+    }
+
+    //还需要处理level的旋转，如果不处理，那么level结合会有明显问题
+    //这个函数会处理旋转的部分
+    //这个函数可能难以理解，需要画图自己进行理解
+    //这个旋转算法是核心部分，也可以自行修改
+    private void AlighTo(SnapPoint _ownSnapPoint, SnapPoint _targetSnapPoint)
+    {
+        //获取旋转的偏移值
+        var rotationOffset =
+            _ownSnapPoint.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+
+        //然后将自身整体level的角度和targetPoint的角度设置为一致
+        transform.rotation = _targetSnapPoint.transform.rotation;
+        transform.Rotate(0, 180, 0);
+
+        transform.Rotate(0, -rotationOffset, 0);
+    }
+
+
+    //进入到另一个snapPoint
+    private void SnapTo(SnapPoint _ownSnapPoint, SnapPoint _targetSnapPoint)
+    {
+        //首先计算偏移值，自身的位置加上这个偏移值，就可以和下一个关卡的出口或者入口对应上
+        var offset = transform.position - _ownSnapPoint.transform.position;
+
+        var newPositon = _targetSnapPoint.transform.position + offset;
+        transform.position = newPositon;
+    }
+
+
+    public SnapPoint GetEnterPoint() => GetSnapPointOfType(SnapPointType.Enter);
+    public SnapPoint GetExitPoint() => GetSnapPointOfType(SnapPointType.Exit);
+
+    private SnapPoint GetSnapPointOfType(SnapPointType _pointType)
+    {
+        SnapPoint[] snapPoints = GetComponentsInChildren<SnapPoint>();
+
+        List<SnapPoint> filteredSnapPoints = new List<SnapPoint>();
+
+        //将指定的type放在这个list里面
+        foreach (SnapPoint snapPoint in snapPoints)
+        {
+            if (snapPoint.pointType == _pointType)
+                filteredSnapPoints.Add(snapPoint);
+        }
+
+        //之后返回一个随机的snapPoint
+        if (filteredSnapPoints.Count > 0)
+        {
+            int randomIndex = Random.Range(0, filteredSnapPoints.Count);
+            return filteredSnapPoints[randomIndex];
+        }
+
+        return null;
+
+    }
+}
