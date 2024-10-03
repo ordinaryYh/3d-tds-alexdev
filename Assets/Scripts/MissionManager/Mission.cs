9bce5100cdf95a82885fa1e66d1097cf4f54c6ba
ordinaryYh <873386843@qq.com>
2024-10-03 13:07:11 +0800

    16-001-done

---
+++ Assets/Scripts/MissionManager/Mission.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//abstract代表是一个抽象类
+//抽象类的内部，函数只有声明，没有实现
+public abstract class Mission : ScriptableObject
+{
+    public string missionName;
+    public string missionDescription;
+
+    public abstract void StartMission();
+    public abstract bool MissionCompleted();
+
+    public virtual void UpdateMission()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MissionManager/MissionManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionManager : MonoBehaviour
+{
+    public static MissionManager instance;
+
+    public Mission currentMission;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        currentMission?.StartMission();
+    }
+
+    private void Update()
+    {
+        if (currentMission != null)
+            currentMission.UpdateMission();
+    }
+
+    private void StartMission() => currentMission.StartMission();
+    public bool MissionCompleted() => currentMission.MissionCompleted();
+
+}
